6ff77426701ebc256178aaeee9e46bf18f7e7e2f
zac-h <89298052+zac-h@users.noreply.github.com>
2022-02-23 22:02:09 +1030

    Added UI

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -21,6 +21,7 @@ function computerPlay(){
 //write a function the plays a single game of rock paper scissors
 //the function should take two parameter: playerSelection (input by player) and computerSelection (value from computerPlay function) and then retrun a string that declares the winner of the round.
 //The function should be case-INSENSITIVE so the user can enter rock, ROCK, RoCK.
+let result;
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
@@ -53,7 +54,7 @@ function playRound(playerSelection, computerSelection){
     }else{
         result = 'Invalid entry, try again.'
     }
-    console.log(result);
+    //console.log(result);
 }
 
 /*const playerSelection = 'rock';
@@ -64,6 +65,7 @@ console.log(playRound(playerSelection, computerSelection))*/
 //write a function called game
 //this function calls the playRound function 5 times  and records the score of the winner
 
+/*
 function game(n){
 
     for(i=0; i<n; i++){
@@ -81,8 +83,56 @@ function game(n){
         console.log('It\'s a tie!');
     }
 }
+*/
 
 let player = 0;
 let computer = 0;
-let numberOfGames = prompt('How many games?');
-game(numberOfGames);
+//let numberOfGames = prompt('How many games?');
+//game(numberOfGames);
+
+/*const rockButton = document.getElementById("rock");
+rockButton.addEventListener('click', () => {playRound(rockButton.id.toLowerCase(), computerPlay())});
+
+const paperButton = document.getElementById("paper");
+paperButton.addEventListener('click', () => {playRound(paperButton.id.toLowerCase(), computerPlay())});
+
+const scissorsButton = document.getElementById("scissors");
+scissorsButton.addEventListener('click', () => {playRound(scissorsButton.id.toLowerCase(), computerPlay())});
+*/
+
+const buttons = document.querySelectorAll('button');
+const results = document.getElementById('results');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+            playRound(button.id.toLowerCase(), computerPlay());
+            const content = document.createElement('div');
+            content.textContent = result;
+            results.appendChild(content);
+            const score = document.createElement('div');
+            score.textContent = `player: ${player} vs. computer: ${computer}`
+            results.appendChild(score);
+
+            if(player == 5 || computer == 5){
+                if(player>computer){
+                    alert('You win!');
+                    results.textContent = '';
+                    player = 0;
+                    computer = 0;
+                } else if(player<computer){
+                    alert('You lose!');
+                    results.textContent = '';
+                    player = 0;
+                    computer = 0;
+                } else {
+                    alert('It\'s a tie!');
+                    results.textContent = '';
+                    player = 0;
+                    computer = 0;
+                }
+            }
+
+    })
+});
+
+
